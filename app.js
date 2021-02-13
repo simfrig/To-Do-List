472809d6ad909438b2b3a22398c8e73c75502e16
simfrig <74631160+simfrig@users.noreply.github.com>
2021-02-12 19:51:07 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const ejs = require('ejs');
+const date = require(__dirname + "/date.js");
+const port = 3000;
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static('public'));
+
+let items = [];
+let workItems = [];
+
+
+app.get('/', function (req, res) {
+let day = date.getDate();
+res.render('list', {listTitle: day,newListItems: items});
+});
+
+app.post('/', (req, res) => {
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect('/work');
+  } else {
+    items.push(item);
+    res.redirect('/');
+  }
+
+});
+
+
+app.get("/work", function (req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.post('/work', (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+
+app.listen(port, () => {
+  console.log("Server is running on port 3000");
+});
